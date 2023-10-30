1d5b90a337568bbbc81359b8975bd61703f7ad9f
sudo takafumi <2111140070x@ed.fuk.kindai.ac.jp>
2023-10-30 10:40:10 +0000

    a

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,26 @@
+class TopController < ApplicationController
+  def main
+    if session[:login_uid] != nil
+      render "main"
+    else
+      render "login"
+    end
+  end
+
+  def login
+    logger.debug params[:uid]
+    if User.find_by(uid: params[:uid]) and User.find_by(pass: params[:pass])
+      session[:login_uid] = params[:uid]
+      redirect_to top_main_path
+      logger.debug("---")
+    else
+      logger.debug("***")
+      render "error"
+    end
+  end
+
+  def logout
+    session.delete(:login_uid)
+    redirect_to top_main_path
+  end
+end
